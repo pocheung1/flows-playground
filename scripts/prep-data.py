046d6308a1e3a24d8ce188e33fc8cd57fdf39224
Po Cheung <po.cheung@dominodatalab.com>
2024-06-14 15:52:09 -0700

    Fail workflow

--- scripts/prep-data.py
+++ scripts/prep-data.py
@@ -14,6 +14,8 @@ named_input = "data_path"
 csv_file = read_input(named_input)
 df = pd.read_csv(csv_file)
 
+raise Exception("prepare data failed")
+
 # Process data
 print(df)
 print("Preparing the data")
