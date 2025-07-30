d1309be542ff98632b7d1dc233641c4636278606
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2025-07-30 10:57:48 -0700

    Pushed from Domino: https://pocheung90200.dmo-team-sandbox.domino.tech/workspace/integration-test/flows-playground?executionId=688a549d9b8e997f68fe08ba

--- math_workflow.py
+++ math_workflow.py
@@ -7,7 +7,7 @@ def math_workflow(a: int, b: int) -> float:
     # Create first task
     add_task = DominoJobTask(
         name='Add numbers',
-        domino_job_config=DominoJobConfig(Command="python add.py"),
+        domino_job_config=DominoJobConfig(Command="python /mnt/code/add.py"),
         inputs={'first_value': int, 'second_value': int},
         outputs={'sum': int},
         use_latest=True
@@ -17,7 +17,7 @@ def math_workflow(a: int, b: int) -> float:
     # Create second task
     sqrt_task = DominoJobTask(
         name='Square root',
-        domino_job_config=DominoJobConfig(Command="python sqrt.py"),
+        domino_job_config=DominoJobConfig(Command="python /mnt/code/sqrt.py"),
         inputs={'value': int},
         outputs={'sqrt': float},
         use_latest=True
